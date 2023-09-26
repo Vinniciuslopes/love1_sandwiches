3f4a04101037e920cf8b6a78ca114a8a0ad97cbf
Vinniciuslopes <>
2023-09-26 10:46:50 +0000

    create function to get numbers of items picked

--- run.py
+++ run.py
@@ -1,3 +1,14 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+data1 = [[280, 300, 320, 320],[270, 280, 260, 270],[320, 310,300, 290]]
+
+def get_data():
+   
+   
+    print("Please enter the number of items picked.")
+    print('Please provide 4 numnber, separate by comma.')
+    print('Example: 290,300,280,310')
+
+    data_str = input('Enter your data: ')
+    print(f'The data provided is {data_str}')
+
+
+get_data()
