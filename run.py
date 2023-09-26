d5c73e0d655da9cfe103e06a0548ed072e9aa493
Vinniciuslopes <>
2023-09-26 18:59:53 +0000

    create validate function

--- run.py
+++ run.py
@@ -1,14 +1,31 @@
 data1 = [[280, 300, 320, 320],[270, 280, 260, 270],[320, 310,300, 290]]
 
+
 def get_data():
-   
    
     print("Please enter the number of items picked.")
     print('Please provide 4 numnber, separate by comma.')
-    print('Example: 290,300,280,310')
+    print('Example: 290,300,280,300\n')
 
     data_str = input('Enter your data: ')
-    print(f'The data provided is {data_str}')
+
+    picked_items = data_str.split(',')
+    validate_data(picked_items)
+
+
+def validate_data(values):
+    
+
+    try:
+        if len(values) != 4:
+            raise ValueError(
+                 f'You have provided {len(values)} numbers, insted of 4 numbers!!'
+        )
+    except ValueError as e:
+        print(f"invalid data: {e}, please try again.")
+
+
 
 
 get_data()
+
